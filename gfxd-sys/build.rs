0c429771d050824c19a22e69ef694b0c5ab7e3da
Anghelo Carvajal <angheloalf95@gmail.com>
2025-10-20 14:08:13 -0300

    submodule libgfxd and build it on build.rs

--- gfxd-sys/build.rs
+++ gfxd-sys/build.rs
@@ -2,5 +2,25 @@
 /* SPDX-License-Identifier: MIT */
 
 fn main() {
-    // TODO
+    static C_PATHS: [&str; 6] = [
+        "vendor/gfxd.c",
+        "vendor/uc_f3d.c",
+        "vendor/uc_f3db.c",
+        "vendor/uc_f3dex.c",
+        "vendor/uc_f3dexb.c",
+        "vendor/uc_f3dex2.c",
+    ];
+    static H_PATHS: [&str; 1] = [
+        "vendor/gfxd.h",
+    ];
+
+    for path in C_PATHS.iter().chain(&H_PATHS) {
+        println!("cargo:rerun-if-changed={path}");
+    }
+
+    cc::Build::new()
+        .files(C_PATHS)
+        .include("vendor")
+        .warnings(false)
+        .compile("gfxd");
 }
